6aa88479727b88d31177e6f22061242e6a2c0800
Nat-crit20 <greuelnathan74@gmail.com>
2023-08-15 12:25:55 -0500

    Added auth for endpoints

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const Board = require("./models/Board");
 const Task = require("./models/Task");
 const Column = require("./models/Column");
 const bodyParser = require("body-parser");
-
+const auth = require("./auth")(app);
+const passport = require("passport");
+require("./passport");
 const app = express();
 const PORT = 3000;
 
@@ -65,231 +67,283 @@ app.get("/users", async (req, res) => {
   res.json(users);
 });
 
-app.get("/user/:userID/board", async (req, res) => {
-  const { userID } = req.params;
-  await User.findOne({ _id: userID }, { Board: 1 })
-    .populate("Board")
-    .then((user) => res.send(user))
-    .catch((err) => res.send(err));
-});
+app.get(
+  "/user/:userID/board",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { userID } = req.params;
+    await User.findOne({ _id: userID }, { Board: 1 })
+      .populate("Board")
+      .then((user) => res.send(user))
+      .catch((err) => res.send(err));
+  }
+);
 
-app.get("/board/:boardID", async (req, res) => {
-  const { boardID } = req.params;
-  await Board.findById({ _id: boardID })
-    .populate("Columns")
-    .then((board) => {
-      res.send(board);
-    })
-    .catch((err) => res.send(err));
-});
-app.get("/column/:columnID", async (req, res) => {
-  const { columnID } = req.params;
-  await Column.findById({ _id: columnID })
-    .populate("Tasks")
-    .then((column) => {
-      res.send(column);
-    })
-    .catch((err) => res.send(err));
-});
-app.post("/user/:userID/board", async (req, res) => {
-  const { Name } = req.body;
-  const { userID } = req.params;
-  const board = await Board.create({
-    Name: Name,
-  });
-  await User.findOneAndUpdate(
-    { _id: userID },
-    { $addToSet: { Board: board._id } },
-    { new: true }
-  )
-    .then((user) => {
-      res.status(200).json(user);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
+app.get(
+  "/board/:boardID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { boardID } = req.params;
+    await Board.findById({ _id: boardID })
+      .populate("Columns")
+      .then((board) => {
+        res.send(board);
+      })
+      .catch((err) => res.send(err));
+  }
+);
+app.get(
+  "/column/:columnID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { columnID } = req.params;
+    await Column.findById({ _id: columnID })
+      .populate("Tasks")
+      .then((column) => {
+        res.send(column);
+      })
+      .catch((err) => res.send(err));
+  }
+);
+app.post(
+  "/user/:userID/board",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { Name } = req.body;
+    const { userID } = req.params;
+    const board = await Board.create({
+      Name: Name,
     });
-});
+    await User.findOneAndUpdate(
+      { _id: userID },
+      { $addToSet: { Board: board._id } },
+      { new: true }
+    )
+      .then((user) => {
+        res.status(200).json(user);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+);
 
-app.post("/board/:boardID/column", async (req, res) => {
-  const { Name } = req.body;
-  const { boardID } = req.params;
+app.post(
+  "/board/:boardID/column",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { Name } = req.body;
+    const { boardID } = req.params;
 
-  const column = await Column.create({
-    Name: Name,
-  });
-
-  await Board.findOneAndUpdate(
-    { _id: boardID },
-    { $push: { Columns: column._id } },
-    { new: true }
-  )
-    .then((board) => {
-      res.status(200).json(board);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
+    const column = await Column.create({
+      Name: Name,
     });
-});
 
-app.put("/column/:columnID", async (req, res) => {
-  const { Name } = req.body;
-  const { columnID } = req.params;
+    await Board.findOneAndUpdate(
+      { _id: boardID },
+      { $push: { Columns: column._id } },
+      { new: true }
+    )
+      .then((board) => {
+        res.status(200).json(board);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+);
 
-  await Column.findOneAndUpdate(
-    { _id: columnID },
-    { $set: { Name: Name } },
-    { new: true }
-  )
-    .then((column) => {
-      res.status(200).json(column);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-app.put("/board/:boardID", async (req, res) => {
-  const { Name } = req.body;
-  const { boardID } = req.params;
+app.put(
+  "/column/:columnID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { Name } = req.body;
+    const { columnID } = req.params;
 
-  await Board.findOneAndUpdate(
-    { _id: boardID },
-    { $set: { Name: Name } },
-    { new: true }
-  )
-    .then((board) => {
-      res.status(200).json(board);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-app.post("/column/:columnID/task", async (req, res) => {
-  const { Title, Description, Status, SubTasks, name } = req.body;
-  const { columnID } = req.params;
-
-  const task = await Task.create({
-    Title,
-    Description,
-    Status: {
-      name,
-      columnID,
-    },
-    SubTasks,
-  });
-
-  await Column.findOneAndUpdate(
-    { _id: columnID },
-    { $push: { Tasks: task._id } },
-    { new: true }
-  )
-    .then((column) => {
-      res.status(200).json(column);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-app.get("/task/:taskID", async (req, res) => {
-  const { taskID } = req.params;
-  await Task.findById({ _id: taskID })
-    .then((task) => {
-      res.send(task);
-    })
-    .catch((err) => res.send(err));
-});
-
-app.put("/column/:columnID/task/:taskID", async (req, res) => {
-  const { columnID, taskID } = req.params;
-  const { Title, Description, SubTasks, name } = req.body;
-  await Task.findById(taskID).then(async (task) => {
-    const columnToRemove = task.Status.columnID;
     await Column.findOneAndUpdate(
-      { _id: columnToRemove },
+      { _id: columnID },
+      { $set: { Name: Name } },
+      { new: true }
+    )
+      .then((column) => {
+        res.status(200).json(column);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+);
+app.put(
+  "/board/:boardID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { Name } = req.body;
+    const { boardID } = req.params;
+
+    await Board.findOneAndUpdate(
+      { _id: boardID },
+      { $set: { Name: Name } },
+      { new: true }
+    )
+      .then((board) => {
+        res.status(200).json(board);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+);
+app.post(
+  "/column/:columnID/task",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { Title, Description, Status, SubTasks, name } = req.body;
+    const { columnID } = req.params;
+
+    const task = await Task.create({
+      Title,
+      Description,
+      Status: {
+        name,
+        columnID,
+      },
+      SubTasks,
+    });
+
+    await Column.findOneAndUpdate(
+      { _id: columnID },
+      { $push: { Tasks: task._id } },
+      { new: true }
+    )
+      .then((column) => {
+        res.status(200).json(column);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+);
+
+app.get(
+  "/task/:taskID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { taskID } = req.params;
+    await Task.findById({ _id: taskID })
+      .then((task) => {
+        res.send(task);
+      })
+      .catch((err) => res.send(err));
+  }
+);
+
+app.put(
+  "/column/:columnID/task/:taskID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { columnID, taskID } = req.params;
+    const { Title, Description, SubTasks, name } = req.body;
+    await Task.findById(taskID).then(async (task) => {
+      const columnToRemove = task.Status.columnID;
+      await Column.findOneAndUpdate(
+        { _id: columnToRemove },
+        { $pull: { Tasks: taskID } },
+        { new: true }
+      );
+    });
+    await Task.findOneAndUpdate(
+      { _id: taskID },
+      {
+        $set: {
+          Title,
+          Description,
+          Status: {
+            name,
+            columnID,
+          },
+          SubTasks,
+        },
+      },
+      { new: true }
+    )
+      .then((task) => {
+        if (task) {
+          res.status(200).send(task);
+        }
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
+);
+
+app.delete(
+  "/column/:columnID/task/:taskID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { columnID, taskID } = req.params;
+    await Column.findOneAndUpdate(
+      { _id: columnID },
       { $pull: { Tasks: taskID } },
       { new: true }
-    );
-  });
-  await Task.findOneAndUpdate(
-    { _id: taskID },
-    {
-      $set: {
-        Title,
-        Description,
-        Status: {
-          name,
-          columnID,
-        },
-        SubTasks,
-      },
-    },
-    { new: true }
-  )
-    .then((task) => {
-      if (task) {
-        res.status(200).send(task);
+    )
+      .then(async (column) => {
+        await Task.findByIdAndDelete(taskID);
+        res.status(200).json(column);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  }
+);
+
+app.delete(
+  "/board/:boardID/column/:columnID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { columnID, boardID } = req.params;
+    try {
+      const column = await Column.findById(columnID);
+      if (!column) {
+        return res.status(404).json({ error: "Column not found" });
       }
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
-});
+      const tasksToDelete = column.Tasks;
+      await Task.deleteMany({ _id: { $in: tasksToDelete } });
 
-app.delete("/column/:columnID/task/:taskID", async (req, res) => {
-  const { columnID, taskID } = req.params;
-  await Column.findOneAndUpdate(
-    { _id: columnID },
-    { $pull: { Tasks: taskID } },
-    { new: true }
-  )
-    .then(async (column) => {
-      await Task.findByIdAndDelete(taskID);
-      res.status(200).json(column);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-app.delete("/board/:boardID/column/:columnID", async (req, res) => {
-  const { columnID, boardID } = req.params;
-  try {
-    const column = await Column.findById(columnID);
-    if (!column) {
-      return res.status(404).json({ error: "Column not found" });
+      const board = await Board.findByIdAndUpdate(
+        boardID,
+        { $pull: { Columns: columnID } },
+        { new: true }
+      );
+      await Column.findByIdAndDelete(columnID);
+      res.status(200).json(board);
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
     }
-    const tasksToDelete = column.Tasks;
-    await Task.deleteMany({ _id: { $in: tasksToDelete } });
-
-    const board = await Board.findByIdAndUpdate(
-      boardID,
-      { $pull: { Columns: columnID } },
-      { new: true }
-    );
-    await Column.findByIdAndDelete(columnID);
-    res.status(200).json(board);
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
   }
-});
+);
 
-app.delete("/user/:userID/board/:boardID", async (req, res) => {
-  const { userID, boardID } = req.params;
-  try {
-    const board = await Board.findById(boardID);
-    const columnsToDelete = board.Columns;
-    await Task.deleteMany({ "Status.ColumnID": { $in: columnsToDelete } });
-    await Column.deleteMany({ _id: { $in: columnsToDelete } });
-    await Board.findByIdAndDelete(boardID);
-    await User.findByIdAndUpdate(
-      userID,
-      { $pull: { Board: boardID } },
-      { new: true }
-    );
-  } catch (error) {
-    res.status(500).json({ error: "Server error" });
+app.delete(
+  "/user/:userID/board/:boardID",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { userID, boardID } = req.params;
+    try {
+      const board = await Board.findById(boardID);
+      const columnsToDelete = board.Columns;
+      await Task.deleteMany({ "Status.ColumnID": { $in: columnsToDelete } });
+      await Column.deleteMany({ _id: { $in: columnsToDelete } });
+      await Board.findByIdAndDelete(boardID);
+      await User.findByIdAndUpdate(
+        userID,
+        { $pull: { Board: boardID } },
+        { new: true }
+      );
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
+    }
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}`);
